33b2fc9e5443520406fc1e66ef03073a7571944e
Chad Dotson <chad@cdotson.com>
2015-07-17 14:23:09 -0500

    Should now work on *nix systems.

--- benchmark.sh
+++ benchmark.sh
@@ -2,6 +2,6 @@
 
 printf "Running $1 $2x - "
 
-for i in {1..$2}; do time $1; done 2>&1 | grep ^real | sed -e s/.*m// | awk '{sum += $1} END {print "average: " sum / NR "s"}'
+for i in {1..$2}; do time -p $1; done 2>&1 | grep ^real | sed -e s/.*m// | awk '{sum += $2} END { print "average: " sum / NR "s"}'
 
 
