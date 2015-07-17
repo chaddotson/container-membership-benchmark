027ebfd60631bed55239c67bd356c5b8ad84199f
Chad Dotson <chad@cdotson.com>
2015-07-17 15:10:17 -0500

    Modifying benchmark's print.

--- benchmark.sh
+++ benchmark.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-printf "Running $1 $2x - "
+printf "$1 $2x - "
 
 for i in {1..$2}; do time -p $1; done 2>&1 | grep ^real | sed -e s/.*m// | awk '{sum += $2} END { print "average: " sum / NR "s"}'
-
-
